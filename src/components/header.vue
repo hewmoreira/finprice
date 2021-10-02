e1ba82748b8a17b7b6dfa30cb980ddd75361efb6
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-02 20:59:46 -0300

    estrutura principal e header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,38 +1,44 @@
 <template>
   <header>
-      <nav class="container">
-          <a href="/" id="logo">FinCa!c</a>
-          <i class="fas fa-ellipsis-h"></i>
-      </nav>
+    <nav class="container">
+      <a href="/"
+        ><img src="../assets/menu.svg" alt="menu" id="menu-button"
+      /></a>
+      <a href="/" id="logo">FinCa1c</a>
+      <span></span>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
+header {
+  background-color: var(--color-background-nav);
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    header{
-        background-color: var(--color-background-nav);
-        height: 60px;
-        width: 100%;
-    }
+nav {
+  display: flex;
+  justify-content: space-between;
+}
 
-    #logo{
-        color: rgb(238, 231, 231);
-        font-weight: 500;
-        font-size: 28px;
-    }
+#logo {
+  color: rgb(238, 231, 231);
+  font-weight: 500;
+  font-size: 28px;
+}
 
-    #nav {
-        display: flex;
-    }
-
-    .fas.fa-bars{
-        width: 300px;
-    }
-
+#menu-button {
+  width: 22px;
+  margin-top: 7px;
+}
 </style>
