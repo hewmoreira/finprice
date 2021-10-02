1326ba7ab3c0bac6d651102954924708f50297d1
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-02 20:29:03 -0300

    removido bootstrap, awesome e sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div class="app">
-    <Sidebar/>
-    <div class="main"></div>
+  <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
-import Sidebar from "./components/sidebar.vue"
+import "./styles/global.css"
+import Header from "./components/header.vue"
 
 
 export default {
   name: 'App',
   components: {
-    Sidebar
+    Header
   }
 }
 </script>
 
 <style>
-  .app{
-    display: flex;
-  }
-  .main{
-    width: 100%;
-    height: 100vh;
-
-    background-color: #f7f7f7;
-  }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+      <nav class="container">
+          <a href="/" id="logo">FinCa!c</a>
+          <i class="fas fa-ellipsis-h"></i>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+    header{
+        background-color: var(--color-background-nav);
+        height: 60px;
+        width: 100%;
+    }
+
+    #logo{
+        color: rgb(238, 231, 231);
+        font-weight: 500;
+        font-size: 28px;
+    }
+
+    #nav {
+        display: flex;
+    }
+
+    .fas.fa-bars{
+        width: 300px;
+    }
+
+</style>
