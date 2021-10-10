baf547269eb7b9e6b297412b76eeb2e5b08b00ab
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-10 17:55:45 -0300

    added home template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
-import "./styles/global.css"
-import Header from "./components/header.vue"
+import "./styles/global.css";
+import Header from "./components/header.vue";
+import Home from "./pages/home.vue";
 
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
