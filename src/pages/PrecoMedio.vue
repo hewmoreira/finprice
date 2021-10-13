d643564c06264790264c1b959d97f4f021c74ef7
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-13 19:20:06 -0300

    looping

--- src/pages/PrecoMedio.vue
+++ src/pages/PrecoMedio.vue
@@ -1,32 +1,52 @@
 <template>
   <main>
-    <div id="title">
-        Calculadora de Preço Médio
-    </div>
+    <div id="title">Calculadora de Preço Médio</div>
     <div id="calculadoraPrecoMedio">
-        <input type="number" class="quantField" placeholder="Quantidade" v-model="quantidade">
-        <input type="number" class="priceField" placeholder="R$ 0,00" v-model="preco">
-        <button type="submit" class="btnCalc" v-if="!firstContact" @click="calcPM" :disabled="!quantidade || !preco">
-            <font-awesome-icon class="iconBtnCalc" icon="plus"/>
-        </button>
-        <button type="submit" class="btnCalc" v-if="firstContact" @click="calcListaGeralPM" :disabled="!quantidade || !preco">
-            <font-awesome-icon class="iconBtnCalc" icon="plus"/>
-        </button>
+      <input
+        type="number"
+        class="quantField"
+        placeholder="Quantidade"
+        v-model="quantidade"
+      />
+      <input
+        type="number"
+        class="priceField"
+        placeholder="R$ 0,00"
+        v-model="preco"
+      />
+      <button
+        type="submit"
+        class="btnCalc"
+        v-if="!firstContact"
+        @click="calcPM"
+        :disabled="!quantidade || !preco"
+      >
+        <font-awesome-icon class="iconBtnCalc" icon="plus" />
+      </button>
+      <button
+        type="submit"
+        class="btnCalc"
+        v-if="firstContact"
+        @click="calcListaGeralPM"
+        :disabled="!quantidade || !preco"
+      >
+        <font-awesome-icon class="iconBtnCalc" icon="plus" />
+      </button>
     </div>
     <div id="result">
-        <div id="divQtdTotal">
-                Quantidade Total: &nbsp;
-                <font-awesome-icon icon="coins"/>&nbsp;
-                <b>{{ formatNumber(quantidade) }}</b>
-        </div>
-        <div id="divPMTotal">
-                Preço Médio: &nbsp;
-                <font-awesome-icon icon="dollar-sign"/>&nbsp;
-                <b>{{ formatPrice(preco) }}</b>
-        </div>
+      <div id="divQtdTotal">
+        Quantidade Total: &nbsp;
+        <font-awesome-icon icon="coins" />&nbsp;
+        <b>{{ formatNumber(quantidade) }}</b>
+      </div>
+      <div id="divPMTotal">
+        Preço Médio: &nbsp;
+        <font-awesome-icon icon="dollar-sign" />&nbsp;
+        <b>{{ formatPrice(preco) }}</b>
+      </div>
     </div>
-    <div id="listaPMs"  v-if="listaQuantidade[0] || listaPreco[0]" >
-        <!-- <div>
+    <div id="listaPMs" v-if="listaQuantidade[0] || listaPreco[0]">
+      <!-- <div>
             <button type="submit" class="btnDelete" :click="deletePM">
                 <font-awesome-icon class="iconBtnDelete" icon="minus"/>
             </button>
@@ -36,23 +56,18 @@
             <b>{{ formatPrice(listaPreco[0]) }}</b>
             {{listaQuantidade}} | {{listaPreco}}
         </div> -->
-        <div>
-            <button type="submit" class="btnDelete" :click="deletePM">
-                <font-awesome-icon class="iconBtnDelete" icon="minus"/>
-            </button>
+      <div v-for="index in listaQuantidade.length" :key="index">
+        <button type="submit" class="btnDelete" :click="deletePM">
+          <font-awesome-icon class="iconBtnDelete" icon="minus" />
+        </button>
 
-            &emsp;<font-awesome-icon icon="coins"/>&nbsp;
-            <b>{{ formatNumber(listaQuantidade[0]) }}</b>&emsp;&emsp;
+        &emsp;<font-awesome-icon icon="coins" />&nbsp;
+        <b>{{ formatNumber(listaQuantidade[index - 1]) }}</b
+        >&emsp;&emsp;
 
-            <font-awesome-icon icon="dollar-sign"/>&nbsp;
-            <b>{{ formatPrice(listaPreco[0]) }}</b>
-            
-            <!-- {{listaQuantidade}} | {{listaPreco}} -->
-            {{listaGeral}}
-        </div>
-        <!-- <div v-for="quants in listaQuantidade" :key="quants">
-            <span>{{quants}}</span>
-        </div> -->
+        <font-awesome-icon icon="dollar-sign" />&nbsp;
+        <b>{{ formatPrice(listaPreco[index - 1]) }}</b>
+      </div>
     </div>
     <div id="publish"></div>
   </main>
@@ -60,190 +75,185 @@
 
 <script>
 export default {
-    name: "PrecoMedio",
-    data(){
-        return{
-            quantidade: '',
-            listaQuantidade: [],
-            preco: '',
-            listaPreco: [],
-            listaGeral: [],
-            firstContact: true
-        }
-    },
-    methods: {
+  name: "PrecoMedio",
+  data() {
+    return {
+      quantidade: "",
+      listaQuantidade: [],
+      preco: "",
+      listaPreco: [],
+      listaGeral: [],
+      firstContact: true,
+    };
+  },
+  methods: {
     formatNumber(value) {
-        let val = (value/1).toFixed(0).replace('.', ',')
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+      let val = (value / 1).toFixed(0).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
     formatPrice(value) {
-        let val = (value/1).toFixed(2).replace('.', ',')
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
-    calcPM(){
-        this.listaQuantidade.push(this.quantidade)
-        this.listaPreco.push(this.preco)
+    calcPM() {
+      this.listaQuantidade.push(this.quantidade);
+      this.listaPreco.push(this.preco);
     },
-    calcListaGeralPM(){
-        this.listaQuantidade.push(this.quantidade)
-        this.listaPreco.push(this.preco)
-        this.listaGeral.push(this.listaQuantidade)
-        this.listaGeral.push(this.listaPreco)
-        this.firstContact = false
+    calcListaGeralPM() {
+      this.listaQuantidade.push(this.quantidade);
+      this.listaPreco.push(this.preco);
+      this.listaGeral.push(this.listaQuantidade);
+      this.listaGeral.push(this.listaPreco);
+      this.firstContact = false;
     }
-}
-}
+  },
+};
 </script>
 
 <style scoped>
+main {
+  background-color: var(--color-background-home);
+  align-items: center;
+}
 
-    main{
-        background-color: var(--color-background-home);
-        align-items: center;
-    }
-    
-    #title{
-        color: var(--color-black2);
-        font-size: 30px;
-        text-align: center;
-        margin-top: 100px;
-    }
+#title {
+  color: var(--color-black2);
+  font-size: 30px;
+  text-align: center;
+  margin-top: 100px;
+}
 
-    #calculadoraPrecoMedio {
-        margin-top: 30px;
-        width: 700px;
-        height: 150px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+#calculadoraPrecoMedio {
+  margin-top: 30px;
+  width: 700px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    .quantField{
-        font-size: 20px;
-        width: 170px;
-        padding: 15px;
-        color: #909090;
-        background: #fff;
-        margin: 0 10px;
-        border: none;
-        border-radius: 5px;
-        box-shadow: 3px 3px 6px rgb(0 0 0 / 5%);
-        transition: 0.5s;
-    }
+.quantField {
+  font-size: 20px;
+  width: 170px;
+  padding: 15px;
+  color: #909090;
+  background: #fff;
+  margin: 0 10px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 3px 3px 6px rgb(0 0 0 / 5%);
+  transition: 0.5s;
+}
 
-    .quantField:hover{
-        width: 230px;
-        transition: 0.5s;
-    }
+.quantField:hover {
+  width: 230px;
+  transition: 0.5s;
+}
 
-    .quantField:focus{
-        width: 230px;
-    }
+.quantField:focus {
+  width: 230px;
+}
 
-    .priceField{
-        font-size: 20px;
-        width: 170px;
-        padding: 15px;
-        color: #909090;
-        background: #fff;
-        margin: 0 10px;
-        border: none;
-        border-radius: 5px;
-        box-shadow: 3px 3px 6px rgb(0 0 0 / 5%);
-        transition: 0.5s;
-    }
+.priceField {
+  font-size: 20px;
+  width: 170px;
+  padding: 15px;
+  color: #909090;
+  background: #fff;
+  margin: 0 10px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 3px 3px 6px rgb(0 0 0 / 5%);
+  transition: 0.5s;
+}
 
-    .priceField:hover{
-        width: 230px;
-    }
+.priceField:hover {
+  width: 230px;
+}
 
-    .priceField:focus{
-        width: 230px;
-    }
+.priceField:focus {
+  width: 230px;
+}
 
-    .btnCalc{
-        width: 50px;
-        height: 47px;
-        margin-left: 15px;
-        font-size: 40px;
-        border: none;
-        border-radius: 5px;
-        display: flex;
-        justify-content: center;
-        color: var(--color-white1);
-        background: var(--color-background-footer);
-        transition: 0.35s;
-        
-    }
+.btnCalc {
+  width: 50px;
+  height: 47px;
+  margin-left: 15px;
+  font-size: 40px;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  color: var(--color-white1);
+  background: var(--color-background-footer);
+  transition: 0.35s;
+}
 
-    .btnCalc:hover{
-        background: var(--color-yellow1);
-        color: var(--color-black2);
-        transition: 0.35s;
-    }
+.btnCalc:hover {
+  background: var(--color-yellow1);
+  color: var(--color-black2);
+  transition: 0.35s;
+}
 
-    .iconBtnCalc{
-        margin-top: 4px;
-        width: 22px;
-    }
+.iconBtnCalc {
+  margin-top: 4px;
+  width: 22px;
+}
 
+#result {
+  min-width: 300px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 10px;
+  border: 2px dashed var(--color-white1);
+}
 
-    #result{
-        min-width: 300px;
-        height: 100px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        padding: 10px;
-        border: 2px dashed var(--color-white1);
-    }
-        
-    .qtdTotal{
-        font-size: 20px;
-    }
+.qtdTotal {
+  font-size: 20px;
+}
 
-    .pmAtual{
-        font-size: 20px;
-    }
+.pmAtual {
+  font-size: 20px;
+}
 
-    #listaPMs{
-        margin-top: 30px;
-        height: 100px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        padding: 10px;
-        border: 2px dashed var(--color-white1);
-    }
+#listaPMs {
+  margin-top: 30px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 10px;
+  border: 2px dashed var(--color-white1);
+}
 
-    .btnDelete{
-        width: 20px;
-        height: 18px;
-        border: none;
-        border-radius: 5px;
-        color: var(--color-white1);
-        background: var(--color-background-footer);
-        transition: 0.35s;
-        
-    }
+.btnDelete {
+  width: 20px;
+  height: 18px;
+  border: none;
+  border-radius: 5px;
+  color: var(--color-white1);
+  background: var(--color-background-footer);
+  transition: 0.35s;
+}
 
-    .btnDelete:hover{
-        background: var(--color-orange1);
-        color: var(--color-black2);
-        transition: 0.35s;
-    }
+.btnDelete:hover {
+  background: var(--color-orange1);
+  color: var(--color-black2);
+  transition: 0.35s;
+}
 
-    .iconBtnDelete{
-        padding: 0;
-        margin-top: 1px;
-        width: 20px;
-    }
+.iconBtnDelete {
+  padding: 0;
+  margin-top: 1px;
+  width: 20px;
+}
 
-    #publish{
-        margin-top: 30px;
-        width: 600px;
-        height: 100px;
-        background: #fff;
-    }
-
+#publish {
+  margin-top: 30px;
+  width: 600px;
+  height: 100px;
+  background: #fff;
+}
 </style>
