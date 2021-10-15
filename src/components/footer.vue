2f67d0a5bfe9a71b71f9245729429c8483da5f58
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-15 17:27:34 -0300

    added links e styles, fontawesome

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,31 @@
 <template>
   <footer>
+      <div id="end">
+          <div>
+          FinCalc
+          </div>
+          
+          <div class="links">
+              <a href="#">
+                Política de Privacidade
+              </a>
+          </div>
 
+          <div class="contato">
+            <a href="https://github.com/hewmoreira" target="_blank">
+              <font-awesome-icon :icon="['fab', 'github']" />
+            </a>
+            <a href="https://gitlab.com/hewmoreira" target="_blank">
+              <font-awesome-icon :icon="['fab', 'gitlab']" />
+            </a>
+            <a href="https://twitter.com/hewmoreira" target="_blank">
+              <font-awesome-icon :icon="['fab', 'twitter']" />
+            </a>
+            <a href="mailto:hewertonfm@gmail.com">
+                <font-awesome-icon icon="envelope" />
+            </a>
+          </div>
+      </div>
   </footer>
 </template>
 
@@ -10,10 +35,51 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 footer{
+    width: 100%;
     height: 60px;
+    background-color: var(--color-black1);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* flex-direction: row; */
+}
+
+#end {
+    /* background-color: blue; */
+    font-size: 15px;
+    width: 100%;
+    margin-left: 25%;
+    margin-right: 25%;
+    display: flex;
+    justify-content: space-between;
+    color: var(--color-white1);
+}
+
+.contato {
+    font-size: 20px;
+    width: 110px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.links {
+    text-decoration: none;
+    color: white;
+
+}
+
+a:-webkit-any-link {
+    color: var(--color-white1);
+    cursor: pointer;
+    text-decoration: none;
+}
+
+a:hover{
+    transition: 0.5s;
+    color: var(--color-yellow1);
 }
 
 </style>
