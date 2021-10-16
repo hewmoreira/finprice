bf15542a931b2b27ced5556ac803a545439a6538
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-16 10:53:11 -0300

    router completed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <PrecoMedio />
+    <router-view />
     <Footer />
   </div>
 </template>
@@ -9,14 +9,12 @@
 <script>
 import "./styles/global.css";
 import Header from "./components/header.vue";
-import PrecoMedio from "./pages/precoMedio/PrecoMedio.vue";
 import Footer from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    PrecoMedio,
     Footer
   }
 }
