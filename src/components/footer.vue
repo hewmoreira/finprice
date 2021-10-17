a3d7a69dbf78f21fed17336b2d592840b57418cd
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-17 14:22:05 -0300

    response Header

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -44,7 +44,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 footer {
   width: 100%;
   height: 60px;
--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -21,7 +21,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #about {
   display: flex;
   flex-direction: column;
