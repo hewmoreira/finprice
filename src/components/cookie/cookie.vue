82234983ab0d5d550d7c2b0f8836fc0d26605a28
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-10-17 16:15:05 -0300

    Remove Readme

---
+++ src/components/cookie/cookie.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="cookie">
+    <button type="button" @click="getCookie()">get {{ result }}</button>
+    <button type="button" @click="addCookie()">add</button>
+    <button type="button" @click="removeCookie()">remove</button>
+  </div>
+</template>
+
+<script>
+import Cookie from './cookie'
+
+export default {
+    name: 'teste',
+    methods: Cookie,
+    data() {
+        return {
+            result: null
+        }
+    },
+    methods: {
+        getCookie() {
+            this.result = Cache.get("MY_KEY");
+        },
+        addCookie() {
+            Cache.add("MY_KEY", { token: "xpto-1234", 3600 });
+        },
+        removeCookie() {
+            Cache.remove("MY_KEY");
+        },
+    },
+    mounted() {
+    
+    }
+}
+</script>
+
+<style>
+</style>
